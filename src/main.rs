85276846699a96e7363d64666118061b4708d702
ayush-sb <ayush.sb2003@gmail.com>
2023-04-27 01:54:15 +0530

    Fix rotation and mirroring

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,19 @@ use std::{env, fs};
 
 fn main() {
     let argv: Vec<String> = env::args().collect();
-    if argv.len() != 2 {
-        println!("usage: {} qoi_filename png_filename", argv[0]);
+    if argv.len() != 2 && argv.len() != 3 {
+        println!("usage: {} qoi_filename [png_filename]", argv[0]);
+        return;
     }
 
-    let qoi_file = String::from(argv[1].clone());
-    let input = fs::read(qoi_file).expect("error: fs::read_to_string");
-
+    let input = fs::read(argv[1].clone()).expect("error: fs::read_to_string");
     let img: ImageBuffer<Rgba<u8>, Vec<u8>> = decode::decode_image(&input);
-    let _ = img.save("/home/ayush/Desktop/kodim23.png");
+    if argv.len() == 2 {
+        let name_length = argv[1].len();
+        let mut name = String::from(&argv[1][0..(name_length - 3)]);
+        name.push_str("png");
+        let _ = img.save(name);
+    } else {
+        let _ = img.save(argv[2].clone());
+    }
 }
